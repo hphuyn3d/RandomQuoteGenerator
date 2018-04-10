00d722580a450dc5f7da858f50d4ae90b6a8906d
hhuynh <hhuynh@connellypartners.com>
2018-04-10 00:13:02 -0400

    Initial project setup

---
+++ random-quote/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { QuoteBoxComponent } from './quote-box/quote-box.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteBoxComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
